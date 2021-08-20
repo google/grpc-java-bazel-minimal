decfb5386fbe4447c6a5cf0f91b1a616d1b3ad5a
Burak Emir <burak.emir@gmail.com>
2021-08-20 17:07:46 +0200

    Use Java 11 and "var" (type inference).

--- src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
+++ src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
@@ -82,7 +82,7 @@ public class HelloWorldServer {
 
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-      HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+      var reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
       responseObserver.onNext(reply);
       responseObserver.onCompleted();
     }
